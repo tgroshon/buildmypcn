fb6c6fbebfefc6cfd7658bf70e98d542c2dcbe97
Tommy Groshong <tgroshong@instructure.com>
2014-11-05 15:30:22 -0700

    Skeleton Grapher Directive for Diagram Rendering
     - Some basic stuff for a Grapher directive: just pass PCN data in attr

---
+++ public/modules/diagrams/directives/grapher.client.directive.js
@@ -0,0 +1,44 @@
+'use strict';
+
+//Diagrams service used to communicate Diagrams REST endpoints
+angular.module('diagrams').directive("grapher", ['$document',
+  function($document) {
+    /**
+     * Do other work in _private functions
+     */
+    function _didWork(val) {
+      console.log(val);
+    };
+
+    /**
+     * Controller to do Scope setup for template
+     */
+    function controller() {
+
+    };
+
+    /**
+     * @scope: Angular scope object.
+     * @element: jqLite-wrapped element that this directive matches.
+     * @attrs: hash object with key-value pairs of normalized attribute names and their corresponding attribute values.
+     */
+    function link(scope, element, attrs) {
+      _didWork(attrs.pcn);
+      scope.pcnString = attrs.pcn;
+      element.bind("mouseenter", function () {
+        alert('Entered with this value: ' + attrs.enter); 
+      });
+    };
+
+    /**
+     * Directive Setup Options
+     */
+    return {
+      restrict: 'E',
+      link: link,
+      controller: controller,
+      // templateUrl: 'sometemplate.html',
+      template: '<div><strong>I am here: {{pcnString}}</strong></div>'
+    };
+  }
+]);
