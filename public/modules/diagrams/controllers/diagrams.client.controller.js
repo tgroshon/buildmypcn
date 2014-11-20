45a4d060688f3234016e295224f7c197123dc4c0
Garett Breen <garett.breen@gmail.com>
2014-11-19 21:22:57 -0700

    Added predecessors

--- public/modules/diagrams/controllers/diagrams.client.controller.js
+++ public/modules/diagrams/controllers/diagrams.client.controller.js
@@ -2,112 +2,140 @@
 
 // Diagrams controller
 angular.module('diagrams').controller('DiagramsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Diagrams', 'Groups', 'PCN',
-	function($scope, $stateParams, $location, Authentication, Diagrams, Groups, PCN) {
-		$scope.authentication = Authentication;
-		Groups.query().$promise.then(function(groups) {
-			if (groups.length > 0) {
-				$scope.selectedGroup = groups[0];
-			}
-			$scope.groups = groups;
-		});
-		$scope.stepTypes = [
-			{'name': 'process', 'displayedName': 'Process'},
-			{'name': 'decision', 'displayedName': 'Decision'},
-			{'name': 'wait', 'displayedName': 'Wait'},
-			{'name': 'divergent_process', 'displayedName': 'Divergent Process'}
-		];
-		$scope.valueSpecificOptions = [-3, -2, -1, 0, 1, 2, 3];
-		$scope.valueGenericOptions = [-3, -2, -1, 0, 1, 2, 3];
+    function ($scope, $stateParams, $location, Authentication, Diagrams, Groups, PCN) {
 
-		// Create a new, blank PCN object
-		$scope.diagram = PCN.initPCN('', '', '');
-		$scope.diagram.domains = [PCN.initDomain('', 'Provider'), PCN.initDomain('', 'Customer')];
-		$scope.diagram.steps = [PCN.initStep($scope.diagram.domains[0], '', '', null)];
+        $scope.authentication = Authentication;
 
-		$scope.addDomain = function() {
-			$scope.diagram.domains.push(PCN.initDomain('', ''));
-		};
-		$scope.addStep = function() {
-			$scope.diagram.steps.push(PCN.initStep($scope.diagram.domains[0], '', '', null));
-		};
+        Groups.query().$promise.then(function (groups) {
+            if (groups.length > 0) {
+                $scope.selectedGroup = groups[0];
+            }
+            $scope.groups = groups;
+        });
 
-		// Create new Diagram
-		$scope.create = function() {
-			// Create new Diagram object
-			var diagram = new Diagrams ({
-        title: this.diagram.metadata.title,
-        group: this.selectedGroup._id,
-				description: this.diagram.metadata.description,
-				domains: this.diagram.domains,
-				steps: this.diagram.steps
-			});
+        $scope.stepTypes = [
+            {'name': 'process', 'displayedName': 'Process'},
+            {'name': 'decision', 'displayedName': 'Decision'},
+            {'name': 'wait', 'displayedName': 'Wait'},
+            {'name': 'divergent_process', 'displayedName': 'Divergent Process'}
+        ];
 
-			// Redirect after save
-			diagram.$save(function(response) {
-				$location.path('diagrams/' + response._id);
+        $scope.predecessorTypes = [
+            {'name': 'normal_relationship', 'displayName': 'Normal'},
+            {'name': 'loose_temporal_relationship', 'displayName': 'Loose Temporal'}
+        ];
 
-				// Clear form fields
-				$scope.title = '';
-				$scope.description = '';
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
+        $scope.valueSpecificOptions = [-3, -2, -1, 0, 1, 2, 3];
+        $scope.valueGenericOptions = [-3, -2, -1, 0, 1, 2, 3];
 
-		// Remove existing Diagram
-		$scope.remove = function( diagram ) {
-			if ( diagram ) { diagram.$remove();
+        // Create a new, blank PCN object
+        $scope.diagram = PCN.initPCN('', '', '');
+        $scope.diagram.domains = [PCN.initDomain('', 'Provider'), PCN.initDomain('', 'Customer')];
+        $scope.diagram.steps = [PCN.initStep($scope.diagram.domains[0], '', '', null)];
 
-				for (var i in $scope.diagrams ) {
-					if ($scope.diagrams [i] === diagram ) {
-						$scope.diagrams.splice(i, 1);
-					}
-				}
-			} else {
-				$scope.diagram.$remove(function() {
-					$location.path('diagrams');
-				});
-			}
-		};
+        $scope.addDomain = function () {
+            $scope.diagram.domains.push(PCN.initDomain('', ''));
+        };
 
-		// Update existing Diagram
-		$scope.update = function() {
-			var diagram = $scope.diagram;
+        $scope.addStep = function () {
+            $scope.diagram.steps.push(PCN.initStep($scope.diagram.domains[0], '', '', null));
+            setPredecessors($scope.diagram);
+        };
+
+        $scope.deleteStep = function (index) {
+            $scope.diagram.steps.splice(index, 1);
+            setPredecessors($scope.diagram);
+        };
+
+        // Create new Diagram
+        $scope.create = function () {
+            // Create new Diagram object
+            var diagram = new Diagrams({
+                title: this.diagram.metadata.title,
+                group: this.selectedGroup._id,
+                description: this.diagram.metadata.description,
+                domains: this.diagram.domains,
+                steps: this.diagram.steps
+            });
+
+            // Redirect after save
+            diagram.$save(function (response) {
+                $location.path('diagrams/' + response._id);
+
+                // Clear form fields
+                $scope.title = '';
+                $scope.description = '';
+            }, function (errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
+
+        // Remove existing Diagram
+        $scope.remove = function (diagram) {
+            if (diagram) {
+                diagram.$remove();
+
+                for (var i in $scope.diagrams) {
+                    if ($scope.diagrams [i] === diagram) {
+                        $scope.diagrams.splice(i, 1);
+                    }
+                }
+            } else {
+                $scope.diagram.$remove(function () {
+                    $location.path('diagrams');
+                });
+            }
+        };
+
+        // Update existing Diagram
+        $scope.update = function () {
+            var diagram = $scope.diagram;
+
+            setPredecessors(diagram);
+
             diagram.group = $scope.selectedGroup;
-			diagram.$update(function() {
-				$location.path('diagrams/' + diagram._id);
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
+            diagram.$update(function () {
+                $location.path('diagrams/' + diagram._id);
+            }, function (errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
 
-    $scope.setEditedGroup = function() {
-      $scope.setEditedGroup = $scope.diagram.group;
-    };
+        function setPredecessors(diagram) {
+            for (var i = 1; i < diagram.steps.length; i++) {
+                var step = diagram.steps[i];
+                var previousStep = diagram.steps[i - 1];
+                step.predecessors = [PCN.initPredecessor(previousStep.id, $scope.predecessorTypes[0].displayName, previousStep.title)]
+            }
+        }
 
-		// Find a list of Diagrams
-		$scope.find = function() {
-			$scope.diagrams = Diagrams.query();
-		};
+        $scope.setEditedGroup = function () {
+            $scope.setEditedGroup = $scope.diagram.group;
+        };
 
-		// Find existing Diagram
-		$scope.findOne = function() {
-			$scope.groups = Groups.query();
-			var promise = Diagrams.get({
-				diagramId: $stateParams.diagramId
-			});
+        // Find a list of Diagrams
+        $scope.find = function () {
+            $scope.diagrams = Diagrams.query();
+        };
 
-      promise.$promise.then(function(diagram) {
-         $scope.diagram = diagram;
-         $scope.selectedGroup = null;
+        // Find existing Diagram
+        $scope.findOne = function () {
+            $scope.groups = Groups.query();
+            var promise = Diagrams.get({
+                diagramId: $stateParams.diagramId
+            });
 
-         for (var i = 0; i < $scope.groups.length; i++) {
-           if ($scope.groups[i]._id === $scope.diagram.group._id) {
-             $scope.selectedGroup = $scope.groups[i];
-             break;
-           }
-         }
-      });
-		};
-	}
+            promise.$promise.then(function (diagram) {
+                $scope.diagram = diagram;
+                $scope.selectedGroup = null;
+
+                for (var i = 0; i < $scope.groups.length; i++) {
+                    if ($scope.groups[i]._id === $scope.diagram.group._id) {
+                        $scope.selectedGroup = $scope.groups[i];
+                        break;
+                    }
+                }
+            });
+        };
+    }
 ]);
