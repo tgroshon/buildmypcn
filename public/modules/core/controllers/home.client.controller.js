04738ba52b7ef8de1a8af1dcbdeb979783016184
Ryan Craig <wilcraig@gmail.com>
2014-12-01 18:13:23 -0700

    Homepage
    Preliminary Homepage

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,9 +1,15 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication','Groups','Diagrams',
+	function($scope, Authentication, Groups, Diagrams) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+		
+		//Load in data for groups & diagrams
+		$scope.find = function() {
+			$scope.groups = Groups.query();
+			$scope.diagrams = Diagrams.query();
+		};
 	}
 ]);
