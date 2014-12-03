cf95653a1182686d24418aa868514719fc571e45
Tommy Groshong <tgroshong@instructure.com>
2014-12-02 23:02:09 -0700

    More fiddling with prod env

--- config/env/production.js
+++ config/env/production.js
@@ -21,7 +21,8 @@ module.exports = {
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
 			]
 		},
-		css: 'public/dist/application.min.css'
+		css: 'public/dist/application.min.css',
+		js: 'public/dist/application.min.js'
 	},
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
